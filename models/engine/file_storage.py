1484f10c3b6c9e13da2b886332b99b9830bbfa92
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-28 23:54:29 +0000

    Some changes in nicolas's branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
-"""[summary]"""
 import json
-from ..base_model import BaseModel 
-class nose:
-    pass
+import models
+from models.base_model import BaseModel
+"""[summary]"""
 
-obj = BaseModel()
-print(obj)
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        FileStorage.__objects["BaseModel." + obj.id] = obj
+    
+    def save(self):
+        with open(FileStorage.__file_path, "a", encoding="utf-8") as file:
+            json.dump(FileStorage.__objects, file)
+    
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                FileStorage.__objects = json.load(file)
+        except:
+            pass
+
--- test_base_model.py
+++ test_base_model.py
@@ -3,10 +3,11 @@ from models.base_model import BaseModel
 
 my_model = BaseModel()
 my_model.name = "Holberton"
+my_model.age = 30
 my_model.my_number = 89
-print(my_model)
+print("\n",my_model)
 my_model.save()
-print(my_model)
+print("\n",my_model)
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
