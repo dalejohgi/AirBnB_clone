6c6f97f697ab47a30b2cadf9325498be7786ab13
David Hincapie <1508@holbertonschool.com>
2020-07-01 14:32:53 +0000

    Fix PEP8 - Add Authors

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""[summary]
+"""[BaseModel Module]
 """
+
 
 import uuid
 import models
@@ -8,49 +9,51 @@ import datetime
 
 
 class BaseModel():
-	"""[summary]
-	"""
+    """
+    [Base Class]
+    """
 
-	def __init__(self, *args, **kwargs):
-		"""[summary]
-		"""
-		if kwargs is not None and len(kwargs) != 0:
-			for key, value in kwargs.items():
-				if key == "__class__":
-					pass
-				elif key == "created_at":
-					self.created_at = datetime.datetime.strptime((kwargs["created_at"]), "%Y-%m-%dT%H:%M:%S.%f")
-				elif key == "updated_at":
-					self.updated_at = datetime.datetime.strptime((kwargs["updated_at"]), "%Y-%m-%dT%H:%M:%S.%f")
-				else:
-					setattr(self, key, value)
+    def __init__(self, *args, **kwargs):
+        """Initializer method"""
 
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.datetime.today()
-			self.updated_at = datetime.datetime.today()
-			models.storage.new(self)
-      
-	def __str__(self):
-		"""[summary]
-		"""
-		return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
-	
-	def save(self):
-		"""[summary]
-		"""
-		models.storage.save()
-		self.updated_at = datetime.datetime.today()
-	
-	def to_dict(self):
-		"""[summary]
-		"""
-		my_dict = self.__dict__.copy()
-		my_dict["__class__"] = type(self).__name__		
-		my_dict["updated_at"] = self.updated_at.isoformat()
-		my_dict["created_at"] = self.created_at.isoformat()
-		#new_dict[__class__] = BaseModel
-		#new_dict[updated_at] = str(self.updated_at)
-		#new_dict[created_at] = str(self.created_at)
-		return my_dict
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "created_at":
+                    self.created_at = (datetime.datetime.strptime(
+                                       (kwargs["created_at"]),
+                                       "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "updated_at":
+                    self.updated_at = (datetime.datetime.strptime(
+                                       (kwargs["updated_at"]),
+                                       "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
 
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.today()
+            self.updated_at = datetime.datetime.today()
+            models.storage.new(self)
+
+    def __str__(self):
+        """__str__ method"""
+
+        return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+
+    def save(self):
+        """Saves an object of BaseModel"""
+
+        models.storage.save()
+        self.updated_at = datetime.datetime.today()
+
+    def to_dict(self):
+        """Return the dictionary representation of an object"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["created_at"] = self.created_at.isoformat()
+
+        return my_dict
--- models/city.py
+++ models/city.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""[City Module]"""
+
+
 from models.base_model import BaseModel
 import models
 
+
 class City(BaseModel):
-    """"""
+    """City Class of HBnB"""
+
     state_id = ""
     name = ""
-    
+
     def __str__(self):
+        """String representation of City instance"""
+
         return ("[City] ({}) {}".format(self.id, self.__dict__))
